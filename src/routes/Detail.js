2a705ea6ece82944e465d2d1bf2633483713599c
Narae Hyeon <narae.hyeon@hhlaw.com.au>
2024-08-07 16:18:21 +1000

    lifecycle, hook 함수

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,28 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function DetailPage ( props ) {
+
+  useEffect (() => {
+    setTimeout( () => {
+      document.querySelector(".alert.alert-warning").style.display='none';
+    }, 2000)
+
+  })
+
   let {id} = useParams();
 
   // Find the item by the parameter id
   let shoesItem = props.shoes.find( function(o) {
     return o.id == id;
   });
-  // let shoesItem = props.shoes.find( (o) => o.id == id );
+  // 짧은버전: let shoesItem = props.shoes.find( (o) => o.id == id );
 
   return (
     <div className="container">
+      <div className="alert alert-warning">2초 뒤 숨겨줫!</div>
       {
-        (id >= 1 && id <=2) ? 
+        (id >= 0 && id <=2) ? 
         (<div className="row">
             <div className="col-md-6">
             <img src={"https://codingapple1.github.io/shop/shoes" + (shoesItem.id+1) + ".jpg"} width="100%" alt="detailedImage"/>
